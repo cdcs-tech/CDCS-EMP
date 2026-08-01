a9277e39cbe54e7215a29a424fbfdef06ddd6aed
cdcd-tech <cdcs.ict@gmail.com>
2026-08-02 01:36:41 +0200

    Sprint 1.6 Stage 1.6.1 - UI asset foundation

---
+++ app/static/js/app.js
@@ -0,0 +1,11 @@
+/*
+====================================================
+CDCS Enterprise Management Platform
+
+Global JavaScript
+====================================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("CDCS-EMP initialized.");
+});
